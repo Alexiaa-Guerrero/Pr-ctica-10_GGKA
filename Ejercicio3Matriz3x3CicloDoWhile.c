7e465f54362036c50e1ca0dd1d5eebf4985ef8ff
Alexiaa-Guerrero <karlaalexia.guerrerogonzalez@gmail.com>
2024-10-18 14:40:26 -0600

    Create Ejercicio3Matriz3x3CicloDoWhile.c

---
+++ Ejercicio3Matriz3x3CicloDoWhile.c
@@ -0,0 +1,17 @@
+/*Karla Alexia Guerrero González
+Práctica 10 17/10/24
+Impresión de matriz 3 x 3 Ciclo Do While*/
+
+#include <stdio.h>
+
+int main()
+{
+  int arr[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+  int i, j;
+
+do
+    {
+        res = res * a;
+        i = i + 1;
+    }
+    while(i < b);
